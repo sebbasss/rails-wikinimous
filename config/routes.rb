af7e1b30cc4b9625bb5afcb380daa15c7c99e540
Sebastian Jara <s.jaranilsson@gmail.com>
2022-05-03 16:47:49 -0400

    funcionalidad lsita

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'articles#index'
+
+  get 'articles/new', to: 'articles#new', as: :new
+  post 'articles', to: 'articles#create'
+
+  get 'articles/:id/edit', to: 'articles#edit', as: :edit
+  patch 'articles/:id', to: 'articles#update'
+
+  get 'articles/:id', to: 'articles#show', as: :article
+
+  delete 'articles/:id', to: 'articles#destroy', as: :destroy
 end
